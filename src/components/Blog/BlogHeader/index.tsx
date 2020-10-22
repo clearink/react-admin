500e731ae0363070894cdc2470a95bfc72e833a0
clearink <1561939638@qq.com>
2020-10-22 09:00:09 +0800

    '前端页面制作准备'

--- src/components/Blog/BlogHeader/index.tsx
+++ src/components/Blog/BlogHeader/index.tsx
@@ -3,13 +3,15 @@ import styles from "./style.module.scss"
 interface IProps {}
 function BlogHeader(props: PropsWithChildren<IProps>) {
 	return (
-		<div className='flex items-center justify-between'>
-			<div>菜单</div>
-			<ul>
-				<li className='inline-block mr-6'>action1</li>
-				<li className='inline-block mr-6'>action2</li>
-				<li className='inline-block mr-6'>action3</li>
-				<li className='inline-block mr-6'>action4</li>
+		<div className='flex items-center justify-between flex-wrap'>
+			<div className='text-2xl text-blue-600'>ClearInk的博客</div>
+			<ul className={styles.action}>
+				{/* 后端返回各个类型 */}
+				<li className={styles.item}>首页</li>
+				<li className={styles.item}>分类</li>
+				<li className={styles.item}>归档</li>
+				<li className={styles.item}>标签</li>
+				<li className={styles.item}>关于</li>
 			</ul>
 		</div>
 	)
