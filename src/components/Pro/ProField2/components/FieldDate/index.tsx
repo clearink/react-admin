80376c5074b0ec3b0046bad647743d52f8f9681e
hx <hx@hx.com>
2020-12-16 17:54:58 +0800

    床位分配

---
+++ src/components/Pro/ProField2/components/FieldDate/index.tsx
@@ -0,0 +1,9 @@
+import React, {memo} from 'react'
+import classNames from 'classnames'
+import styles from './style.module.scss'
+
+// 日期相关
+function FieldDate() {
+   return <div></div>
+}
+export default memo(FieldDate)
